f196b8cc7dafc07e24fd89ae36b2e9e9a215c623
yarmolovichalex <yarmolovichalex@gmail.com>
2014-11-23 14:14:26 +0300

    context added

---
+++ HallOfFame-dotnet/Infrastructure/AlbumContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+using HallOfFame_dotnet.Models;
+
+namespace HallOfFame_dotnet.Infrastructure
+{
+    public class AlbumContext : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+    }
+}
