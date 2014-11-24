ad6fb4a77b4e92bde8f7f26d59d92854fd1cf2d3
yarmolovichalex <yarmolovichalex@gmail.com>
2014-11-24 22:19:22 +0300

    get album info from lastfm

--- HallOfFame_dotnet/Controllers/AlbumController.cs
+++ HallOfFame_dotnet/Controllers/AlbumController.cs
@@ -1,6 +1,11 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Configuration;
 using System.Web.Mvc;
 using HallOfFame_dotnet.Infrastructure;
+using HallOfFame_dotnet.Utils;
 
 namespace HallOfFame_dotnet.Controllers
 {
@@ -8,9 +13,23 @@ namespace HallOfFame_dotnet.Controllers
     {
         private readonly AlbumContext context = new AlbumContext();
 
-        public ActionResult Index(int id)
+        public async Task<ActionResult> Index(int id)
         {
             var album = context.Albums.First(a => a.ID == id);
+
+            var uri = new Uri(WebConfigurationManager.AppSettings["lastfmApi"])
+                .AddQuery("method", "album.getInfo")
+                .AddQuery("artist", album.Artist)
+                .AddQuery("album", album.Name)
+                .AddQuery("api_key", WebConfigurationManager.AppSettings["lastfmKey"]);
+
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(uri);
+
+            string resp = await response.Content.ReadAsStringAsync();
+
+           
+            
             return View(album);
         }
 	}
