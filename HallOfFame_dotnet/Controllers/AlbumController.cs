32798b7f75022aa97db7d6150e29157d73a4ce91
yarmolovichalex <yarmolovichalex@gmail.com>
2014-11-28 00:36:30 +0300

    error handling

--- HallOfFame_dotnet/Controllers/AlbumController.cs
+++ HallOfFame_dotnet/Controllers/AlbumController.cs
@@ -39,8 +39,13 @@ namespace HallOfFame_dotnet.Controllers
                     .AddQuery("api_key", WebConfigurationManager.AppSettings["lastfmKey"]);
 
             var response = await new HttpClient().GetAsync(uri).Result.Content.ReadAsStringAsync();
+            XDocument doc = XDocument.Parse(response);
 
-            Album album = ParseResponse(response);
+            string status = doc.Root.Attribute("status").Value;
+            if (status == "failed")
+                return new ContentResult { Content = doc.Root.Element("error").Value }; // TODO переделать
+
+            Album album = ParseResponse(doc);
 
             // TODO errors handling
 
@@ -56,32 +61,30 @@ namespace HallOfFame_dotnet.Controllers
             return RedirectToAction("Index", new { id = album.ID });
         }
 
-        private Album ParseResponse(string response)
+        private Album ParseResponse(XDocument doc)
         {
-            XDocument doc = XDocument.Parse(response);
-
-            string artist = (string) doc.Root.Element("album").Element("artist");
+            string artist = (string)doc.Root.Element("album").Element("artist");
             string albumName = (string)doc.Root.Element("album").Element("name");
 
             int? year = null;
             DateTime releaseDate;
-            DateTime.TryParse((string) doc.Root.Element("album").Element("releasedate"), out releaseDate);
+            DateTime.TryParse((string)doc.Root.Element("album").Element("releasedate"), out releaseDate);
             if (releaseDate != DateTime.MinValue)
                 year = releaseDate.Year;
 
             // TODO выбирать картинки не больше 600 x 600 px
             var imageElement = doc.Root.Descendants("image")
                     .FirstOrDefault(e => e.Attribute("size") != null && e.Attribute("size").Value == "mega");
-            string image = imageElement != null ? (string) imageElement : null;
+            string image = imageElement != null ? (string)imageElement : null;
 
             var descElement = doc.Root.Element("album").Element("wiki");
-            string description = descElement != null ? (string) descElement.Element("summary") : null;
+            string description = descElement != null ? (string)descElement.Element("summary") : null;
 
             XElement tracksElement = doc.Root.Element("album").Element("tracks");
             List<Track> tracks = tracksElement.Elements().Select(track => new Track
             {
-                Number = int.Parse(track.Attribute("rank").Value), 
-                Name = track.Element("name").Value, 
+                Number = int.Parse(track.Attribute("rank").Value),
+                Name = track.Element("name").Value,
                 Duration = int.Parse(track.Element("duration").Value)
             }).ToList();
 
