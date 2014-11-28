fadd4ccace8b6ecf5ef38a3fe36be20cf10f8d76
Alexander Yarmolovich <yarmolovichalex@gmail.com>
2014-11-28 18:25:46 +0300

    data annotations

--- HallOfFame_dotnet/Models/Album.cs
+++ HallOfFame_dotnet/Models/Album.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace HallOfFame_dotnet.Models
 {
@@ -7,10 +9,17 @@ namespace HallOfFame_dotnet.Models
     {
         public int ID { get; set; }
         [DisplayName("Album name")]
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Artist { get; set; }
+        [Required]
+        [Range(1900, 2015, ErrorMessage = "Invalid year!")] // TODO
         public int? Year { get; set; }
+        [Required]
+        [Url(ErrorMessage = "Invalid URL!")]
         public string Image { get; set; }
+        [Required]
         public virtual ICollection<Track> Tracklist { get; set; }
         //public List<string> Tags { get; set; } 
         public string Description { get; set; }
@@ -19,7 +28,9 @@ namespace HallOfFame_dotnet.Models
     public class Track
     {
         public int ID { get; set; }
+        [Required]
         public int Number { get; set; }
+        [Required]
         public string Name { get; set; }
         public int Duration { get; set; }
         public int AlbumID { get; set; }
