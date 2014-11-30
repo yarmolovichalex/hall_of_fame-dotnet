b816b6ce7087b5617db44b0f3803c9240bf19d95
yarmolovichalex <yarmolovichalex@gmail.com>
2014-11-30 15:27:38 +0300

    styling

--- HallOfFame_dotnet/Models/Album.cs
+++ HallOfFame_dotnet/Models/Album.cs
@@ -6,14 +6,18 @@ namespace HallOfFame_dotnet.Models
     public class Album
     {
         public int ID { get; set; }
-        [DisplayName("Album name")]
+        [DisplayName("Album name:")]
         public string Name { get; set; }
+        [DisplayName("Artist:")]
         public string Artist { get; set; }
+        [DisplayName("Year:")]
         public int? Year { get; set; }
+        [DisplayName("Iamge:")]
         public string Image { get; set; }
         //public List<string> Tags { get; set; } 
+        [DisplayName("Description:")]
         public string Description { get; set; }
-
+        [DisplayName("Tracklist:")]
         public virtual ICollection<Track> Tracklist { get; set; }
     }
 
